cd24f828ea0b3bff8d43bc15e0732b74eb8631a7
San <dinhduc2550@gmail.com>
2022-11-26 11:34:19 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./auth/login/login.component";
 import {RegisterComponent} from "./auth/register/register.component";
+import {PostsComponent} from "./posts/posts.component";
+import {PostDetailComponent} from "./posts/post-detail/post-detail.component";
+import {PostEditComponent} from "./posts/post-edit/post-edit.component";
+import {PostListComponent} from "./posts/post-list/post-list.component";
+import {PostResolverService} from "./posts/post-resolver.service";
 import {SearchComponent} from "./search/search.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {ErrorPageComponent} from "./error-page/error-page.component";
@@ -11,9 +16,11 @@ import {UserInfoComponent} from "./profile/user-info/user-info.component";
 import {PasswordComponent} from "./profile/password/password.component";
 import {HistoryBookingComponent} from "./history-booking/history-booking.component";
 import {BookingComponent} from "./booking/booking.component";
+import {HasRoleHostGuard} from "./guard/has-role-host.guard";
 import {AccountDetailComponent} from "./admin/manager-account/account-detail/account-detail.component";
 import {PaymentComponent} from "./payment/payment.component";
 import {PaymentSuccessComponent} from "./payment-success/payment-success.component";
+
 
 const appRoute: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
@@ -35,29 +42,51 @@ const appRoute: Routes = [
       {path: 'password', component: PasswordComponent},
       {path: 'history-booking', component: HistoryBookingComponent}
     ]
+
   },
-  // {
-  //   path: 'posts', children: [
-  //     {path: '', component: PostsComponent},
-  //     {path: 'post-detail/:id', component: PostDetailComponent, resolve: [PostResolverService]},
-  //     {path: 'post-edit', component: PostEditComponent, canActivate: [HasRoleHostGuard]},
-  //     {path: 'post-edit/:id', component: PostEditComponent, canActivate: [HasRoleHostGuard]},
-  //
-  //     {path: 'post-list', component: PostListComponent}
-  //
-  //   ]
-  // },
+  // {path: 'login', component: LoginComponent},
+  // {path: 'register', component: RegisterComponent},
   {
-    path: 'posts', loadChildren: ()=>import('./posts/post-routing.module').then(m=>m.PostRoutingModule)
+    path: 'posts', children: [
+      {path: '', component: PostsComponent},
+      {path: 'post-detail/:id', component: PostDetailComponent, resolve: [PostResolverService]},
+
+      // children:[
+      //   {path: ':id',component: PostDetailComponent}
+      // ]
+      {path: 'post-edit', component: PostEditComponent, canActivate: [HasRoleHostGuard]},
+      {path: 'post-edit/:id', component: PostEditComponent, canActivate: [HasRoleHostGuard]},
+
+      {path: 'post-list', component: PostListComponent}
+
+    ]
   },
   {
     path: 'hosts',
     loadChildren: () => import('./host/host-routing.module').then(m => m.HostRoutingModule)
+    // {path: '', component: HostComponent},
+    // {path: 'host-post-list', component: HostPostListComponent},
+    // {
+    //   path: 'manage-rate', children: [
+    //     {path: '', component: ManageRateComponent},
+    //     {path: 'rate-detail', component: RateDetailComponent}
+    //   ]
+    // },
+    // {path: 'manage-voucher', component: ManageVoucherComponent}
 
   },
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin-routing.module').then(m => m.AdminRoutingModule)
+    // component:AdminComponent, children: [
+    // {path: '',redirectTo:'manager-account/manager-account-host',pathMatch:'full'},
+    // {path: 'manager-account',component: ManagerAccountHostComponent,children: [
+    //     {path: 'manager-account-host',component: ManagerAccountHostComponent},
+    //     {path: 'manager-account-customer',component: ManagerAccountCustomerComponent},
+    //   ]},
+    //
+    //
+    // ]
   },
   {path: 'search', component: SearchComponent},
   {path: 'error', component: ErrorPageComponent},
